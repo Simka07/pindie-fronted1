f7ace1e5a40b883a8c7121bd671caf921f99f466
SimkaMari <masha@simonenko.ru>
2024-05-23 14:54:36 +0300

    корс исправлен

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://front-pindie-web.nomoredomainswork.ru/api' 
+export const BASE_URL = 'https://pindie-web.nomoredomainswork.ru/api' 
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
